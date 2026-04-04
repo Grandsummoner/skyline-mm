8edf42f514e071142aff21e46487bd055e277f63
Grandsummoner <gsnextninja@gmail.com>
2026-04-04 08:44:31 +0800

    Create plugin.hpp

---
+++ src/plugin.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "rack.hpp"
+
+using namespace rack;
+
+// Declare your plugin instance
+extern Plugin* pluginInstance;
+
+// Declare one line per module you add later
+// Example: extern Model* modelMyModule;
+extern Model* modelMyModule;
