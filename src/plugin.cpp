d308146f4d15e29eb3d5fd3be4653702cf8e203e
Grandsummoner <gsnextninja@gmail.com>
2026-05-24 11:38:58 +0800

    Update plugin.cpp

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -2,13 +2,6 @@
 
 Plugin* pluginInstance;
 
-float maybeDefaultContrast = 255.0f;
-int maybeDefaultTheme = -1;
-
-void maybeSaveSettings() {}
-void maybeApplyContrastToAll(float contrast) { maybeDefaultContrast = contrast; }
-void maybeApplyThemeToAll(int theme) { maybeDefaultTheme = theme; }
-
 void init(Plugin* p) {
     pluginInstance = p;
     p->addModel(modelSkyline);
