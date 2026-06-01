7a8f1b90f4beaa06335330e6918a8892003f812e
Grandsummoner <gsnextninja@gmail.com>
2026-06-01 16:21:31 +0800

    Add files via upload

--- src/Skyline.cpp
+++ src/Skyline.cpp
@@ -64,6 +64,7 @@ struct Skyline : Module {
         DIVIDE_PARAM,
         ATTENUATE_PARAM,
         OFFSET_PARAM,
+        CLK_SWITCH_PARAM,  // 0=CLK 1=CV 2=SLAVE
         // Mode buttons
         MUTE_PARAM,
         LENGTH_PARAM,
@@ -152,6 +153,7 @@ struct Skyline : Module {
         getParamQuantity(DIVIDE_PARAM)->snapEnabled = true;
         configParam(ATTENUATE_PARAM, 0.f, 1.f,  1.f,  "Attenuate");
         configParam(OFFSET_PARAM,   -5.f, 5.f,  0.f,  "Offset", "V");
+        configSwitch(CLK_SWITCH_PARAM, 0.f, 2.f, 0.f, "Clock Mode", {"CLK", "CV", "SLAVE"});
 
         configButton(MUTE_PARAM,   "Mute Mode");
         configButton(LENGTH_PARAM, "Length Mode");
@@ -303,6 +305,9 @@ struct Skyline : Module {
         }
 
         // ---- Reset / Hold ----
+        // CLK/CV/SLAVE switch
+        int clkMode = (int)params[CLK_SWITCH_PARAM].getValue();
+        
         bool holdHigh = inputs[RESET_INPUT].getVoltage() > 1.0f;
         if (resetTrig.process(inputs[RESET_INPUT].getVoltage())) {
             for (int ch = 0; ch < 8; ch++) seqPos[ch] = 0;
@@ -311,7 +316,14 @@ struct Skyline : Module {
 
         // ---- Clock ----
         bool clocked = false;
-        if (!holdHigh && clockTrig.process(inputs[CLOCK_INPUT].getVoltage())) {
+        
+        if (clkMode == 1) {
+            // CV mode: use clock input voltage to select step position
+            float cvVoltage = inputs[CLOCK_INPUT].getVoltage();
+            int newPos = (int)(clamp(cvVoltage / 10.f, 0.f, 1.f) * 15.f);
+            for (int ch = 0; ch < 8; ch++) seqPos[ch] = newPos;
+        }
+        else if (!holdHigh && clkMode == 0 && clockTrig.process(inputs[CLOCK_INPUT].getVoltage())) {
             int div = (int)params[DIVIDE_PARAM].getValue();
             divCount++;
             if (divCount >= div) {
@@ -448,6 +460,34 @@ struct Skyline : Module {
         j = json_object_get(root, "chanMuted");
         if (j) for(int ch=0;ch<8;ch++) chanMuted[ch]=json_boolean_value(json_array_get(j,ch));
     }  // end dataFromJson
+    void onRandomize(const RandomizeEvent& e) override {
+        // Only randomize CV step values - not mode/button states
+        for (int ch = 0; ch < 8; ch++)
+            for (int s = 0; s < 16; s++)
+                stepCV[ch][s] = random::uniform() * 5.f;
+    }
+
+    void onReset(const ResetEvent& e) override {
+        Module::onReset(e);
+        // Clear all step data
+        for (int ch = 0; ch < 8; ch++) {
+            for (int s = 0; s < 16; s++) {
+                stepCV[ch][s]    = 0.f;
+                stepMuted[ch][s] = false;
+                stepSmooth[ch][s]= false;
+            }
+            seqLength[ch]  = 16;
+            seqPos[ch]     = 0;
+            direction[ch]  = 0;
+            scaleIndex[ch] = 0;
+            chanMuted[ch]  = false;
+            frozen[ch]     = false;
+            glideCV[ch]    = 0.f;
+        }
+        selectedChan = 0;
+        divCount     = 0;
+    }
+
 };  // end Skyline struct
 
 struct SkylineWidget : ModuleWidget {
@@ -473,6 +513,7 @@ struct SkylineWidget : ModuleWidget {
         // CLK/CV y=48.09, RST/HLD y=61.97
         addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.79f, 48.09f)), module, Skyline::CLOCK_INPUT));
         addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.79f, 61.97f)), module, Skyline::RESET_INPUT));
+        addParam(createParamCentered<CKSSThree>(mm2px(Vec(16.5f, 53.84f)), module, Skyline::CLK_SWITCH_PARAM));
 
         // Knobs y=53.84
         addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(33.19f, 53.84f)), module, Skyline::OFFSET_PARAM));
@@ -482,18 +523,18 @@ struct SkylineWidget : ModuleWidget {
         // Mode buttons row 1 y=46.05
         addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<RedLight>>>(
             mm2px(Vec(77.21f, 46.05f)), module, Skyline::MUTE_PARAM,   Skyline::MUTE_LIGHT));
-        addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<WhiteLight>>>(
+        addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<YellowLight>>>(
             mm2px(Vec(87.03f, 46.05f)), module, Skyline::LENGTH_PARAM, Skyline::LENGTH_LIGHT));
-        addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<WhiteLight>>>(
+        addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<YellowLight>>>(
             mm2px(Vec(96.17f, 46.05f)), module, Skyline::SHIFT_PARAM,  Skyline::SHIFT_LIGHT));
 
         // Mode buttons row 2 y=59.94
-        addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<WhiteLight>>>(
+        addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<YellowLight>>>(
             mm2px(Vec(77.21f, 59.94f)), module, Skyline::SCALE_PARAM,  Skyline::SCALE_LIGHT));
-        addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<GreenLight>>>(
-            mm2px(Vec(87.03f, 59.94f)), module, Skyline::SAVE_PARAM,   Skyline::SAVE_LIGHT));
-        addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<GreenLight>>>(
-            mm2px(Vec(96.17f, 59.94f)), module, Skyline::RECALL_PARAM, Skyline::RECALL_LIGHT));
+        addParam(createParamCentered<VCVButton>(
+            mm2px(Vec(87.03f, 59.94f)), module, Skyline::SAVE_PARAM));
+        addParam(createParamCentered<VCVButton>(
+            mm2px(Vec(96.17f, 59.94f)), module, Skyline::RECALL_PARAM));
 
         // 8 sliders - BefacoSlidePot is 8.59px wide, fits in 20HP
         // BefacoSlidePot is 104px tall (~35mm) - centred in slider area
