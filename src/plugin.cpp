b4af26445120e4ad428da80339e2f8b8ca3c1759
Grandsummoner <gsnextninja@gmail.com>
2026-06-20 11:52:50 +0800

    Update plugin.cpp

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,8 +1,11 @@
 #include "plugin.hpp"
 
+// Initialize the plugin instance
 Plugin* pluginInstance;
 
 void init(Plugin* p) {
     pluginInstance = p;
+
+    // Register our models
     p->addModel(modelSkyline);
 }
